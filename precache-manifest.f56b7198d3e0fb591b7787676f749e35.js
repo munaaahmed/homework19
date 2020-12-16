791f91da26987c6c453d920ff7141233914be997
Muna Ahmed <munaahmed@Munas-MacBook-Air.local>
2020-12-15 20:43:03 -0800

    Updates

---
+++ precache-manifest.f56b7198d3e0fb591b7787676f749e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518e8436db41b7d8af73e8210a3bd736",
+    "url": "/homework19/index.html"
+  },
+  {
+    "revision": "234f320e9c48c7aeb9e5",
+    "url": "/homework19/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "234f320e9c48c7aeb9e5",
+    "url": "/homework19/static/js/2.434f0efe.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/homework19/static/js/2.434f0efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e7abaf3539b4b97035",
+    "url": "/homework19/static/js/main.1b8504f6.chunk.js"
+  },
+  {
+    "revision": "81e1b8a9eb2d61776814",
+    "url": "/homework19/static/js/runtime-main.9c16967b.js"
+  }
+]);
